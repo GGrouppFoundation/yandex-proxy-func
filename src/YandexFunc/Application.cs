0b372dc66ed92cd6545d501f8cc42d6a8d739982
pmosk <pm@ggroupp.ru>
2025-07-11 12:21:56 +0300

    Use Yandex.Http.Token

--- src/YandexFunc/Application.cs
+++ src/YandexFunc/Application.cs
@@ -11,7 +11,7 @@ public static class Application
         =>
         PrimaryHandler.UseStandardSocketsHttpHandler()
         .UseLogging("ProxyApi")
-        .UseYandexIamToken("YANDEX_OAUTH_TOKEN")
+        .UseYandexIamToken("Yandex")
         .UsePollyStandard()
         .UseHttpApi()
         .UseProxySendHandler();
